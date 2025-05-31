edbe564c6c5b987275a4ae5c4756a1e3e6e80afa
Stuart van der Lee <stuartvanderlee92+github@gmail.com>
2025-05-31 11:41:05 +0200

    Add initial install script (#1)
    * Add initial install script and list of apps
    
    * Add git config
    
    * Update
    
    * Update README
    
    * Rename and update apps
    
    * Update README

---
+++ init-script.zsh
@@ -0,0 +1,20 @@
+# Remove all items from Dock and set the icon size to 48
+defaults write "com.apple.dock" "persistent-apps" -array; killall Dock
+defaults write com.apple.dock tilesize -int 48; killall Dock
+print "Dock setup done"
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+xargs brew install < app-list.txt
+
+# git config
+git config --global user.name "Stuart van der Lee"
+git config --global user.email [FILL IN YOUR EMAIL]
+git config --global push.default "current"
+git config --global push.autoSetupRemote true
+git config --global pull.rebase false
+git config --global pull.prune true
+git config --global core.editor "code"
+git config --global init.defaultBranch "main"
+print "Git config done"
